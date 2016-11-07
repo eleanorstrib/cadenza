0cd9d77f8148d4731b7e45a2972c9c44f62edb24
eleanorstrib <hello@eleanorstrib.com>
2016-11-06 21:56:21 -0800

    updated views file to use django-twilio features, deleted url

--- motif/urls.py
+++ motif/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^monkey/$', views.hello_monkey, name='hello_monkey'),
 ]
--- motif/views.py
+++ motif/views.py
@@ -4,10 +4,8 @@ from django.http import HttpResponse
 def index(request):
     return(HttpResponse("index page!"))
 
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
-
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
-    print("sending")
-    return str(resp)
+@twilio_view
+def sms_test(request):
+    r=Response()
+    r.message("thanks for your text :)")
+    return r
