b17c3f81fa29f5161f48296791d2632c641484f2
eleanorstrib <hello@eleanorstrib.com>
2016-11-09 16:56:27 -0800

    forgot import statement in views.py

--- motif/views.py
+++ motif/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django_twilio.decorators import twilio_view
 
 def index(request):
     return(HttpResponse("index page!"))
