b808fbe05282a118de5967c7a949386e97235d64
eleanorstrib <hello@eleanorstrib.com>
2016-11-13 15:47:57 -0800

    trying with csrf exempt decorator

--- motif/views.py
+++ motif/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from twilio import twiml
 from twilio.twiml import Response
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return(HttpResponse("index page!"))
 
+@csrf_exempt
 def sms(request):
     r = Response()
     r.message("hello there!")
