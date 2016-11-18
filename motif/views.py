04584243ee625ec8b8dcff4bbe8fd945ed325d89
eleanorstrib <hello@eleanorstrib.com>
2016-11-17 17:11:20 -0800

    reinstall django-twilio -- updated views & requirements

--- motif/views.py
+++ motif/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from twilio import twiml
-from twilio.twiml import Response
-from django.views.decorators.csrf import csrf_protect
+from django_twilio.decorators import twilio_view
 
 def index(request):
     return(HttpResponse("index page!"))
 
-@csrf_protect
+@twilio_view
 def sms(request):
-    r = Response()
+    r = twiml.Response()
     r.message("hello there!")
     return r
