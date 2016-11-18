e164f062745f78e56bdde940df6941b49fd9828f
eleanorstrib <hello@eleanorstrib.com>
2016-11-17 17:31:40 -0800

    attempting a bug fix

--- cadenza/settings.py
+++ cadenza/settings.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cadenza-motif.herokuapp.com', 'localhost']
 
