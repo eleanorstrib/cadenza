c837db06b62f611b9e30483a47fea2e9a475f8c8
eleanorstrib <hello@eleanorstrib.com>
2016-11-17 17:19:10 -0800

    updated urls and views

--- motif/views.py
+++ motif/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from twilio import twiml
+from twilio.twiml import Response
 from django_twilio.decorators import twilio_view
 
 def index(request):
