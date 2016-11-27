c05c4c4affb5b3e91b5a671a38297639779c9539
eleanorstrib <hello@eleanorstrib.com>
2016-11-27 12:31:45 -0800

    trying adding sms to main view

--- motif/views.py
+++ motif/views.py
@@ -4,11 +4,12 @@ from twilio import twiml
 from twilio.twiml import Response
 from django_twilio.decorators import twilio_view
 
-def index(request):
-    return(HttpResponse("index page!"))
-
 @twilio_view
-def sms(request):
+def index(request):
     r = twilio.twiml.Response()
     r.message("hello there!")
     return r
+    # return(HttpResponse("index page!"))
+#
+# @twilio_view
+# def sms(request):
