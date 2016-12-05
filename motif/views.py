584aa801293cd6224b4abba2688927d86fc521c3
eleanorstrib <hello@eleanorstrib.com>
2016-12-04 21:28:31 -0800

    imported emojiand altered code to send back result

--- motif/views.py
+++ motif/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from twilio import twiml
 from twilio.twiml import Response
 from django_twilio.decorators import twilio_view
+import emoji
 
 def index(request):
     return(HttpResponse("index page!"))
@@ -10,10 +11,7 @@ def index(request):
 @twilio_view
 def sms(request):
     user_msg = request.POST.get('Body', '')
-    if user_msg == ':)':
-        msg = "glad to hear you're feeling :)"
-    else:
-        msg = "hello there!"
+    msg = user_msg
     r = twiml.Response()
     r.message(msg)
     return r
