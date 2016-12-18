4eb8567a44a8d7681b979da0856a08557810a6b8
eleanorstrib <hello@eleanorstrib.com>
2016-12-17 16:59:10 -0800

    changing static in settings.py

--- cadenza/settings.py
+++ cadenza/settings.py
@@ -112,11 +112,11 @@ DJANGO_TWILIO_FORGERY_PROTECTION = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    [os.path.join(BASE_DIR, 'static')]
+    [os.path.join(PROJECT_ROOT, 'static')]
 )
 print("STATICFILES_DIRS", STATICFILES_DIRS)
 print("STATIC_ROOT", STATIC_ROOT)
