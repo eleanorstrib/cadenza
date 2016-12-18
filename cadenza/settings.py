1fbe35b6b15caa5fb28124c6d39e266b4f91b415
eleanorstrib <hello@eleanorstrib.com>
2016-12-17 17:00:34 -0800

    changing static in settings.py

--- cadenza/settings.py
+++ cadenza/settings.py
@@ -116,10 +116,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    [os.path.join(PROJECT_ROOT, 'static')]
+    [os.path.join(BASE_DIR, 'static')],
 )
-print("STATICFILES_DIRS", STATICFILES_DIRS)
-print("STATIC_ROOT", STATIC_ROOT)
-print("templte dirs", TEMPLATES[0]['DIRS'])
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
